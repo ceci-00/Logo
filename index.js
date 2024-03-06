485077c53e0eb1def64ebd5c548c9ae4facfb2a7
ceci-00 <ceci.ga003@gmail.com>
2024-03-06 00:21:23 -0600

    added questions array to index.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 // runs application using imports from lib
-const shapeInput = require('./lib/shapes');
+const { readFile, writeFile } = require('fs/promises');
+const Shape = require('./lib/shapes');
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter text for your logo (up to 3 characters).',
+        name: 'text'
+    },
+    {
+        type: 'input',
+        message: 'Enter text color (color keyword or hexadecimal number)',
+        name: 'text-color',
+    },
+    {
+        type: 'list',
+        message: 'Choose a shape',
+        choices: ['circle', 'triangle', 'square'],
+        name: 'chosen-shape',
+    },
+    {
+        type: 'input',
+        message: 'Enter shape color (color keyword or hexadecimal number)',
+        name: 'shape-color',
+    },
+];
+
